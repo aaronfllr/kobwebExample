9212644010295bbadf94f46245c9bad376d5d7c5
aaron <45025327+aaronfllr@users.noreply.github.com>
2023-10-15 19:04:54 -0400

    Playground

---
+++ site/src/jsMain/kotlin/org/example/app/components/sections/Footer.kt
@@ -0,0 +1,30 @@
+package org.example.app.components.sections
+
+import androidx.compose.runtime.*
+import com.varabyte.kobweb.compose.css.AlignSelf
+import com.varabyte.kobweb.compose.foundation.layout.Row
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.silk.components.navigation.Link
+import com.varabyte.kobweb.silk.components.style.*
+import com.varabyte.kobweb.silk.components.text.SpanText
+import com.varabyte.kobweb.silk.theme.SilkTheme
+import com.varabyte.kobweb.silk.theme.colors.palette.border
+import com.varabyte.kobweb.silk.theme.colors.palette.toPalette
+import org.jetbrains.compose.web.css.*
+
+val FooterStyle by ComponentStyle.base {
+    Modifier
+        .margin(top = 2.cssRem)
+        .borderTop(1.px, LineStyle.Solid, colorMode.toPalette().border)
+        .padding(topBottom = 1.cssRem, leftRight = 4.cssRem)
+        .alignSelf(AlignSelf.Center)
+}
+
+@Composable
+fun Footer(modifier: Modifier = Modifier) {
+    Row(FooterStyle.toModifier().then(modifier)) {
+        SpanText("Made with ")
+        Link("https://github.com/varabyte/kobweb", "Kobweb")
+    }
+}
---
+++ site/src/jsMain/kotlin/org/example/app/components/widgets/GoHomeLink.kt
@@ -0,0 +1,7 @@
+package org.example.app.components.widgets
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.silk.components.navigation.Link
+
+@Composable
+fun GoHomeLink() = Link("/", "Go Home")
